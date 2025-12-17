9f25a9a4cbc4944f438b2387eba2e35719a76291
TenisFederations <tenisgithub@mail.ru>
2025-12-17 10:56:05 +0300

    123

--- kfn_tennis/news/models.py
+++ kfn_tennis/news/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 from django.utils import timezone
 from django.urls import reverse
-
+from django_ckeditor_5.fields import CKEditor5Field
 class News(models.Model):
     title = models.CharField(verbose_name='Заголовок', max_length=255)
     slug = models.SlugField(unique=True)
-    content = models.TextField(verbose_name='Текст')
+    full_description = CKEditor5Field(
+    verbose_name='Полное описание',
+    config_name='default'
+)
     image = models.ImageField(upload_to='news/', blank=True, null=True, verbose_name='Изображение')
     created_at = models.DateTimeField(default=timezone.now, verbose_name='Дата создания')
     published = models.BooleanField(default=True, verbose_name='Опубликовано')
